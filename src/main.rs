b0d661902a28af7945ff1127a72a0fb65e9c571d
Ben <ben.arteii@proton.me>
2024-03-14 00:32:19 +0100

    thats so weird :cry: its working soo....

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::{env, fs::File, io::prelude::*, path::PathBuf, process};
 
 use color_eyre::eyre::{eyre, Result};
-use poem::{endpoint::StaticFilesEndpoint, EndpointExt, listener::TcpListener, Route};
 use poem::middleware::Cors;
+use poem::{endpoint::StaticFilesEndpoint, listener::TcpListener, EndpointExt, Route};
 use poem_openapi::OpenApiService;
-use tracing::{debug, error, info, Level, subscriber::set_global_default};
+use tracing::{debug, error, info, subscriber::set_global_default, Level};
 use tracing_subscriber::FmtSubscriber;
 
 use crate::api::routes::Api;
@@ -35,8 +35,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // base path for repositories
     let base_path = "E:/RepoTests/Repos";
 
+    let api_name = "Git";
+
     // Initialize API service
-    let api_service = OpenApiService::new(Api::new(base_path), "Git", "1.0").server("/api");
+    let api_service = OpenApiService::new(Api::new(base_path), api_name, "1.0").server("/api");
 
     // check if --static-docs flag is provided
     if let Some(arg) = env::args_os().nth(1) {
@@ -57,6 +59,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut redoc_file = File::create(&redoc_file_path)?;
             redoc_file.write_all(redoc_html_content.as_bytes())?;
 
+            // Generate the HTML content
+            let html_content = format!(r#"<html><head><title>{}</title><style>body{{font-family:Arial,sans-serif;display:flex;flex-direction:column;align-items:center;justify-content:center;height:100vh;margin:0;}}.button{{padding:20px 40px;font-size:18px;margin:10px;border:none;cursor:pointer;border-radius:8px;background-color:#007bff;color:white;text-align:center;text-decoration:none;transition:background-color 0.3s;}}.button:hover{{background-color:#0056b3;}}</style></head><body><h1>Welcome to {} API Documentation</h1><a href="redoc.html" class="button">Redoc</a><a href="swagger_ui.html" class="button">Swagger UI</a></body></html>"#, api_name, api_name);
+            let mut file = File::create("index.html")?;
+            file.write_all(html_content.as_bytes())?;
+
             // Log the generation of Swagger UI and Redoc HTML files
             info!("Swagger UI & Redoc HTML files generated");
 
@@ -64,7 +71,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             return Ok(());
         }
     }
-    
+
     // Define application routes
     let app: Route = Route::new()
         .nest("/redoc", api_service.redoc())
