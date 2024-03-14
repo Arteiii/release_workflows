cffeb8a4953aac1a4625a8cc8d757a648b412b6c
Ben <ben.arteii@proton.me>
2024-03-14 18:16:12 +0100

    code cleanup in main file

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,17 @@ use crate::api::routes::Api;
 mod api;
 mod build;
 mod git;
-
 mod util;
+
+// TODO: remove its jsut for dev
+const BASE_PATH: &str = "E:/RepoTests/Repos";
+const API_NAME: &str = "Git";
+const SWAGGER_UI_FILE: &str = "swagger_ui.html";
+const REDOC_FILE: &str = "redoc.html";
+const INSTALLATION_FILE: &str = "installation.html";
+const LICENSE_FILE: &str = "license.html";
+const README_FILE: &str = "README.md";
+const INDEX_FILE: &str = "index.html";
 
 fn setup_tracing() {
     let subscriber = FmtSubscriber::builder()
@@ -25,31 +34,21 @@ fn setup_tracing() {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Initialize logger
     setup_tracing();
     info!("Startup!");
 
-    // Install error handling
     color_eyre::install()?;
     debug!("Eyre installed");
 
-    // Base path for repositories
-    let base_path = "E:/RepoTests/Repos";
+    let api_service = OpenApiService::new(Api::new(BASE_PATH), API_NAME, "1.0").server("/api");
 
-    let api_name = "Git";
-
-    // Initialize API service
-    let api_service = OpenApiService::new(Api::new(base_path), api_name, "1.0").server("/api");
-
-    // Check if --static-docs flag is provided
     if let Some(arg) = env::args_os().nth(1) {
         if arg == "--static-docs" {
-            generate_static_docs(&api_service, api_name)?;
+            generate_static_docs(&api_service)?;
             return Ok(());
         }
     }
 
-    // Define application routes
     let app: Route = Route::new()
         .nest("/redoc", api_service.redoc())
         .nest("/docs", api_service.swagger_ui())
@@ -58,10 +57,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "/",
             StaticFilesEndpoint::new("src/web/")
                 .show_files_listing()
-                .index_file("index.html"),
+                .index_file(INDEX_FILE),
         );
 
-    // Run the server
     if let Err(err) = poem::Server::new(TcpListener::bind("0.0.0.0:8080"))
         .run(app)
         .await
@@ -73,61 +71,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-/// Generate static documentation files
-fn generate_static_docs(api_service: &OpenApiService<Api, ()>, api_name: &str) -> Result<()> {
-    // Generate the Swagger UI and Redoc HTML content
+fn generate_static_docs(api_service: &OpenApiService<Api, ()>) -> Result<()> {
     let swagger_ui_html_content = api_service.swagger_ui_html();
     let redoc_html_content = api_service.redoc_html();
 
-    // Write the Swagger UI HTML content to file
-    let mut swagger_file = File::create("swagger_ui.html")?;
-    swagger_file.write_all(swagger_ui_html_content.as_bytes())?;
+    write_to_file(SWAGGER_UI_FILE, &swagger_ui_html_content)?;
+    write_to_file(REDOC_FILE, &redoc_html_content)?;
 
-    // Write the Redoc HTML content to file
-    let mut redoc_file = File::create("redoc.html")?;
-    redoc_file.write_all(redoc_html_content.as_bytes())?;
-
-    // Convert Installation instructions Markdown to HTML
     let installation_md_content = include_str!("../Installation.md");
     let installation_html_content = markdown_to_html_with_line_breaks(installation_md_content)?;
-    // Write the Installation HTML content to file
-    let mut installation_file = File::create("installation.html")?;
-    installation_file.write_all(installation_html_content.as_bytes())?;
+    write_to_file(INSTALLATION_FILE, &installation_html_content)?;
 
-    // Convert License Markdown to HTML
     let license_md_content = include_str!("../LICENSE.md");
     let license_html_content = markdown_to_html_with_line_breaks(license_md_content)?;
-    // Write the License HTML content to file
-    let mut license_file = File::create("license.html")?;
-    license_file.write_all(license_html_content.as_bytes())?;
+    write_to_file(LICENSE_FILE, &license_html_content)?;
 
-    // Read the content of README.md
     let readme_content = include_str!("../README.md");
-    // Convert README Markdown to HTML
     let readme_html_content = markdown_to_html_with_line_breaks(readme_content)?;
 
-    // Generate the HTML content from the template
     let html_template = include_str!("web/index_template.html");
     let mut handlebars = Handlebars::new();
     handlebars.register_template_string("index_template", html_template)?;
     let mut data = std::collections::BTreeMap::new();
-    data.insert("api_name", api_name);
+    data.insert("api_name", API_NAME);
     data.insert("readme", &readme_html_content);
     let html_content = handlebars.render("index_template", &data)?;
 
-    // Write the rendered HTML content to the index.html file
-    let mut file = File::create("index.html")?;
-    file.write_all(html_content.as_bytes())?;
+    write_to_file(INDEX_FILE, &html_content)?;
 
     info!("Static documentation files generated");
 
     Ok(())
 }
 
-/// Convert Markdown to HTML with preserved line breaks
+fn write_to_file(file_name: &str, content: &str) -> Result<()> {
+    let mut file = File::create(file_name)?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
+}
+
 fn markdown_to_html_with_line_breaks(markdown: &str) -> Result<String> {
     let parser = Parser::new_ext(markdown, Options::all());
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
-    Ok(html_output.replace("\n", "<br>")) // replace newline characters with <br> tags
+    Ok(html_output.replace("\n", "<br>"))
 }
